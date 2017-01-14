307657c584180e57679b6b388c04e479c3d6ad03
ediber <edibersh@gmail.com>
2017-01-14 19:10:50 +0200

    Initial commit

---
+++ app/src/main/java/edi/com/myfirebasecloudemessaging3/MyFirebaseInstanceIdService.java
@@ -0,0 +1,26 @@
+package edi.com.myfirebasecloudemessaging3;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Edi on 1/14/2017.
+ */
+
+public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("edi ", "Refreshed token: " + refreshedToken);
+
+        // TODO: Implement this method to send any registration to your app's servers.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String refreshedToken) {
+
+    }
+}
